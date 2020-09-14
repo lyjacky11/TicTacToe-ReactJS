d3bcc2ae60229f9231f7061d7c04d82140544d32
lyjacky11 <lyjacky@live.com>
2020-09-14 17:12:08 -0400

    Updates

---
+++ precache-manifest.93b3d6878d825c6199d59b33c38fcfa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d9bec8703156f4ebc5d9db0e97f306",
+    "url": "/TicTacToe-ReactJS/index.html"
+  },
+  {
+    "revision": "b9169e3dc7e5d99df09a",
+    "url": "/TicTacToe-ReactJS/static/css/main.9757e45a.chunk.css"
+  },
+  {
+    "revision": "5fa3ff0b5b25f966a348",
+    "url": "/TicTacToe-ReactJS/static/js/2.2c2a61e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe-ReactJS/static/js/2.2c2a61e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9169e3dc7e5d99df09a",
+    "url": "/TicTacToe-ReactJS/static/js/main.3174dd79.chunk.js"
+  },
+  {
+    "revision": "7d41ad9299d1b95faf7a",
+    "url": "/TicTacToe-ReactJS/static/js/runtime-main.56eb9bfb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TicTacToe-ReactJS/static/media/logo.5d5d9eef.svg"
+  }
+]);
